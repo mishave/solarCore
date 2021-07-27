5bd896722a09f8583c7c5e046d3706fa720138ea
Michael Vernon <61336815+mishave@users.noreply.github.com>
2021-07-27 14:57:10 +1000

    Update main.py

--- main.py
+++ main.py
@@ -29,14 +29,7 @@ def readOneWire():
 def main():
     time_stamp = time()
     tReadings = readOneWire()
-    """ test temperature sensors
-	x = DS18B20()
-	count=x.device_count()
-	i = 0
-	while i < count:
-		print(x.tempC(i))
-		i += 1
-	test"""
+
     writeSheet(time_stamp, tReadings[0], tReadings[1])
 
 
